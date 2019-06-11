6fb095a7bea15cda66d4ddb4a2212ba1fdac17f7
fenavillarroel <fenavillarroel@gmail.com>
2019-06-11 00:30:40 -0400

    first commit

---
+++ frontend/app.js
@@ -0,0 +1,43 @@
+require('./styles/app.css');
+import UI from './UI';
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const ui = new UI();
+  ui.renderBooks();
+})
+
+document.getElementById('book-form')
+  .addEventListener('submit', e => {
+    const title = document.getElementById('title').value;
+    const author = document.getElementById('author').value;
+    const isbn = document.getElementById('isbn').value;
+    const image = document.getElementById('image').files;
+
+
+    const formData = new FormData();
+    formData.append('image', image[0]);
+    formData.append('title', title);
+    formData.append('author', author);
+    formData.append('isbn', isbn);
+
+    const ui = new UI();
+
+    //const book = new Book(title, author, isbn);
+
+    ui.addNewBook(formData);
+    ui.renderMessage('New Book Added','success', 3000);
+    e.preventDefault();
+  });
+
+  document.getElementById('books-card')
+  .addEventListener('click', e => {
+    if (e.target.classList.contains('delete')) {
+      const ui = new UI();
+      ui.deleteBook(e.target.getAttribute('_id'));
+      ui.renderMessage('Book Deleted','danger', 3000);
+      
+      //console.log(e.target.getAttribute('_id'));
+    }
+    e.preventDefault();
+  });
